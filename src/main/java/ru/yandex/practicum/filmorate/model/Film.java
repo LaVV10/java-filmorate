d3a3ed05e9d4d6ce9b5a72f228674ed64fa17bca
LaVV10 <vit.laz2@mail.ru>
2025-11-24 18:52:50 +0300

    ФЗ 10 фикс (добавил validation)

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+
 import java.time.LocalDate;
 
 @Data
